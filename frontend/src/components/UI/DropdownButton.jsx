fd405cdf0e2fe3740789d73a76020dd7185033a6
Artem Klushin <arklual1@gmail.com>
2022-11-01 13:30:05 +0300

    update background and socials style

--- frontend/src/components/UI/DropdownButton.jsx
+++ frontend/src/components/UI/DropdownButton.jsx
@@ -14,14 +14,17 @@ const DropdownButton = props => {
         <button type="button" className="btn dropdownBtnOutline" data-bs-toggle="dropdown" data-bs-auto-close="inside" aria-expanded="false">...</button>
         <ul className="dropdown-menu dropdown-back">
           <li><a className="dropdown-item on-cursor" href="#">Language</a></li>
+          <hr></hr>
           <li><a className="dropdown-item on-cursor" href="#">Theme</a></li>
+          <hr></hr>
           <li><a className="dropdown-item on-cursor" href="#">Whitepaper</a></li>
+          <hr></hr>
           <li><hr className="dropdown-divider"/></li>
           <li>
             <p className="dropdown-item links">
-              <a className="social-media" href="#"><SiDiscord/></a>
-              <a className="social-media" href="#"><SiTelegram/></a>
-              <a className="social-media" href="#"><SiGithub/></a>
+              <a className="social-media" href="#"><SiDiscord size={35}/></a>
+              <a className="social-media" href="#"><SiTelegram size={35}/></a>
+              <a className="social-media" href="#"><SiGithub size={35}/></a>
             </p>
           </li>
         </ul>
